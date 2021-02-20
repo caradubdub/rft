38074e8b341ce33c17bacf9af7d517627e4ad61d
Cara Dibdin <cdibdin@gmail.com>
2021-02-20 14:04:01 -0500

    add server

---
+++ App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Tree from "react-d3-tree";
+import { render } from "react-dom";
+import orgChart from "./index";
+
+export default function Fetchtree() {
+  console.log("render tree");
+  return (
+    // `<Tree />` will fill width/height of its container; in this case `#treeWrapper`.
+    <div id="treeWrapper" style={{ width: "80vw", height: "20vh" }}>
+      <Tree data={orgChart} orientation={"vertical"} />
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+render(<Fetchtree />, rootElement);
---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+const PORT = 8080;
+
+app.use(express.static(path.join(__dirname, "./public")));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.listen(PORT, () => {
+  console.log(`running on port: ${PORT}`);
+});
