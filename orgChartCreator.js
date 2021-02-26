eeea178d31fdacee8fa959de573723278667672a
Cara Dibdin <cdibdin@gmail.com>
2021-02-26 17:03:27 -0500

    add react tree

--- orgChartCreator.js
+++ orgChartCreator.js
@@ -1,27 +1,29 @@
 import fiberwalker from "./fiberwalker";
 import componentObj from "./componentStore";
+import React from "react";
+import Tree from "react-d3-tree";
 
-export default function orgChartCreator() {
-  console.log('hello', componentObj);
-  let orgChart;
-
-  let __ReactFiberDOM;
-  const devTools = global.__REACT_DEVTOOLS_GLOBAL_HOOK__;
-
-  devTools.onCommitFiberRoot = (function (original) {
-    return function (...args) {
-      __ReactFiberDOM = args[1];
-      console.log("dom: ", __ReactFiberDOM.current);
-      orgChart = fiberwalker(__ReactFiberDOM.current, componentObj);
-      console.log("orgChart: ", orgChart);
-
-      return original(...args);
-    };
-  })(devTools.onCommitFiberRoot);
-  return orgChart;
+export default function Fetchtree() {
+  return (
+    // `<Tree />` will fill width/height of its container; in this case `#treeWrapper`.
+    <div id="treeWrapper" style={{ width: "80vw", height: "80vh" }}>
+      <Tree data={orgChart} orientation={"vertical"} />
+    </div>
+  );
 }
 
+let orgChart;
 
+let __ReactFiberDOM;
+const devTools = global.__REACT_DEVTOOLS_GLOBAL_HOOK__;
 
+devTools.onCommitFiberRoot = (function (original) {
+  return function (...args) {
+    __ReactFiberDOM = args[1];
+    console.log("dom: ", __ReactFiberDOM.current);
+    orgChart = fiberwalker(__ReactFiberDOM.current, componentObj);
+    console.log("orgChart: ", orgChart);
 
-
+    return original(...args);
+  };
+})(devTools.onCommitFiberRoot);
