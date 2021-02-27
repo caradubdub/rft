f773a82567cbf24e32453ac91d4863a74eec4ff8
Trevor Carr <trevor.a.carr@gmail.com>
2021-02-27 13:13:33 -0500

    updated orgChartCreator logic

--- orgChartCreator.js
+++ orgChartCreator.js
@@ -1,10 +1,8 @@
 import fiberwalker from "./fiberwalker";
 import componentObj from "./componentStore";
 
-export default function orgChartCreator() {
-  console.log('hello', componentObj);
-  let orgChart;
 
+const orgChartCreator = (orgChart) => {
   let __ReactFiberDOM;
   const devTools = global.__REACT_DEVTOOLS_GLOBAL_HOOK__;
 
@@ -12,15 +10,16 @@ export default function orgChartCreator() {
     return function (...args) {
       __ReactFiberDOM = args[1];
       console.log("dom: ", __ReactFiberDOM.current);
-      orgChart = fiberwalker(__ReactFiberDOM.current, componentObj);
+      console.log('inside onCommit', componentObj);
+      orgChart['renderTree'] = fiberwalker(__ReactFiberDOM.current, componentObj);
       console.log("orgChart: ", orgChart);
 
       return original(...args);
     };
   })(devTools.onCommitFiberRoot);
-  return orgChart;
 }
 
+export default orgChartCreator;
 
 
 
